5155bc2aa2ef6f73b49981fdef9d38d6563a975a
uliseshm <ulises.h.dev@gmail.com>
2024-03-28 21:57:03 -0600

    primer commit

---
+++ login-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      //crear formulario de login
+      //capturar los datos con useState y validarlos
+      //con una DB local, archivo .js
+    </>
+  )
+}
+
+export default App
